43429b79cc23d28b6c8b11cdea8e01075c885f98
Kardtcharsigonist <tamalsarkartwoooo@gmail.com>
2026-05-31 16:01:58 +0530

    feat: update database connection to use pg and adjust dependencies

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,5 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
-const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
 
-export const db = drizzle(sql, { schema });
